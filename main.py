53a25eac1ecc895a578dc236b7263b9314960fd0
Brad Tucker <brad.victor.tucker@gmail.com>
2019-04-12 09:49:41 -0700

    implement cell based random generation

--- main.py
+++ main.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python3
 import numpy
 
-#constants
-view = range(-2, 3)
-earshot = range(-4, 5)
-deadzone = 1
-start = [12, 25]
+#world constants
+grid = 5
+view = 15
+viewAngle = 0.5*numpy.pi
+viewInputs = 16
+smell = 10
+smellInputs = 16
+speed = 1
+speedAngle = viewAngle/2
+pos = [0, 0]
+angle = 0
+chanceGood = .5
+chanceBad = .25
+world = {}
+
+#processing constants
+display = (1 + max(view, smell)//grid) * grid
+viewStep = viewAngle / viewInputs
+smellStep = 2*numpy.pi / smellInputs
+
+#network constants
 numpy.random.seed(12345)
 alpha = .001
-dimX = 33
+dimX = viewInputs + smellInputs
 dimY = 2
 dimR = 4
 dimZ = 10
 dimIn = dimX + dimY + dimR
-
-#build array of world map
-world = numpy.ones((25, 50))
-
-'''
-        [[ 0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 0, 0,   0, 0, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 0, 0, 0,   0, 0, 0, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 0, 0, 0,   0, 0, 0, 1,   1, 1, 1, 0],
-
-        [  0, 1, 1, 1,   1, 0, 0, 0,   0, 0, 0, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 0, 0, 0,   0, 0, 0, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 0, 0,   0, 0, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 1,   1, 1, 1, 0],
-        [  0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0]]
-'''
-
-#set location data
-pos = [start[0], start[1]]
 
 #mainatin record of last input, output, reward and state
 lastX = numpy.zeros((dimX))
@@ -52,124 +40,85 @@ lastZ = numpy.zeros((dimZ))
 Wi = 2 * numpy.random.rand(dimZ, dimZ+dimIn+1) -1
 Wo = 2 * numpy.random.rand(dimIn, dimZ+1) -1
 
-#convert a vector into a cardnial direction
-def cardinal(Y):
-    if numpy.linalg.norm(Y) < deadzone:
-        return [ 0, 0]
-    angle = numpy.arctan2(Y[1], Y[0])
-    if -7*numpy.pi/8 <= angle < -5*numpy.pi/8:
-        return [-1,-1]
-    elif -5*numpy.pi/8 <= angle < -3*numpy.pi/8:
-        return [ 0,-1]
-    elif -3*numpy.pi/8 <= angle < -numpy.pi/8:
-        return [ 1,-1]
-    elif -numpy.pi/8 <= angle < numpy.pi/8:
-        return [ 1, 0]
-    elif numpy.pi/8 <= angle < 3*numpy.pi/8:
-        return [ 1, 1]
-    elif 3*numpy.pi/8 <= angle < 5*numpy.pi/8:
-        return [ 0, 1]
-    elif 5*numpy.pi/8 <= angle < 7*numpy.pi/8:
-        return [-1, 1]
-    else:
-        return [-1, 0]
-
 #render output of world
 def draw():
-    for x in range(len(world)):
-        string = ''
-        for y in range(len(world[0])):
-            if pos[0] == x and pos[1] == y:
-                string += '@ '
-            elif world[x][y] == -1:
-                string+='# '
-            elif world[x][y] == -2:
-                string+='* '
-            elif world[x][y] == 1:
-                string+='. '
-            elif world[x][y] == 0:
-                string+='  '
+    #print inputs and outputs
+    print('X: ' + str(lastX))
+    print('R: ' + str(lastR))
+    print('Y: ' + str(lastY))
+    print('Z: ' + str(lastZ))
+
+    #find current cell
+    x = pos[0]//grid
+    y = pos[1]//grid
+
+    #draw world
+    for i in range(x - display, x + display, grid):
+        #find row text
+        row = '   '
+        for j in range(y - display, y + display, grid):
+            cell = getCell(i, j)
+            if cell == None:
+                row += '  '
+            elif cell[1]:
+                row += '. '
             else:
-                string+=str(world[x][y])
-        print('| ' + string + '|')
+                row += '* '
+        print(row)
 
-#activation function
-def nonLinearFunction(x):
-    if x>0:
-        return numpy.log(1+x)
-    else:
-        return -numpy.log(1-x)
-nlf = numpy.vectorize(nonLinearFunction)
-
-#derivative of the activation function
-def nonLinearDerivative(x):
-    if x>0:
-        return 1/(1+x)
-    else:
-        return 1/(1-x)
-nld = numpy.vectorize(nonLinearDerivative)
+#manage angle math
+def angleSum(a1, a2=0):
+    theta = a1 + a2
+    while theta > numpy.pi:
+        theta -= 2*numpy.pi
+    while theta <= -numpy.pi:
+        theta += 2*numpy.pi
+    return theta
 
 #process an output to compute the new inputs and rewards
 def process(Y):
-    #allow update of position
+    #allow update of angle and position
+    global angle
     global pos
 
-    #calculate movement direction
-    direction = cardinal(Y)
+    #update position with capped speed
+    v = Y[0]
+    if v > speed:
+        v = speed
+    elif v < -speed:
+        v = -speed
+    pos = [pos[0] + v*numpy.cos(angle), pos[0] + v*numpy.sin(angle)]
 
-    #move
-    lastPos = pos
-    newPos = [pos[0] + direction[0], pos[1] + direction[1]]
-    if  0 > newPos[0] or 0 > newPos[1] or len(world) <= newPos[0] or len(world[0]) <= newPos[1]:
-        newPosValue = -1
-    else:
-        newPosValue = world[newPos[0]][newPos[1]]
-    if newPosValue >= 0:
-        pos = newPos
-        world[newPos[0]][newPos[1]] = 0
+    #update angle with capped angular speed
+    a = Y[1]
+    if a > speedAngle:
+        a = speedAngle
+    elif a < -speedAngle:
+        a = -speedAngle
+    angle = angleSum(angle, a)
 
     ### X ###
 
-    #get values of the world map in view
+    #calculate smell and sight inputs
     X1 = []
-    for i in view:
-        for j in view:
-            if  0 > pos[0]+i or 0 > pos[1]+j or len(world) <= pos[0]+i or len(world[0]) <= pos[1]+j:
-                value = -1
-            else:
-                value = world[pos[0] + i][pos[1] + j]
-            X1.append(value)
+    X2 = []
+    
+    #find current cell
+    x = pos[0]//grid
+    y = pos[1]//grid
 
-    #note nearby preditors and prey
-    pred = []
-    prey = []
+    #loop over cells to percieve
+    for i in range(x - display, x + display, grid):
+        for j in range(y - display, y + display, grid):
+            #get cell information
+            cell = getCell(i, j)
 
-    #search for preditors and prey in earshot
-    for i in earshot:
-        for j in earshot:
-            if  0 > pos[0]+i or 0 > pos[1]+j or len(world) <= pos[0]+i or len(world[0]) <= pos[1]+j:
-                value = -1
-            else:
-                value = world[pos[0] + i][pos[1] + j]
-            if value == 1:
-                prey.append([i, j])
-            elif value == -2:
-                pred.append([i, j])
+            #skip if cell has no information
+            if cell != None:
+                #calculate cell inverse distance from
+                cell[0]
 
-    #find closest preditor in earshot
-    closePred = [[0, 0], 0]
-    for p in pred:
-        norm = numpy.linalg.norm(p)
-        if 1/norm > closePred[1]:
-            closePred = [p, 1/norm]
-
-    #find closest prey in earshot
-    closePrey = [[0, 0], 0]
-    for p in prey:
-        norm = numpy.linalg.norm(p)
-        if 1/norm > closePrey[1]:
-            closePrey = [p, 1/norm]
-
+    '''
     #record nearest preditor and prey
     X2 = [closePred[0][0], closePred[0][1], closePrey[0][0], closePrey[0][1]]
 
@@ -181,12 +130,14 @@ def process(Y):
 
     #put all inputs together
     X = numpy.concatenate((X1, X2, X3, X4))
+    '''
 
     ### R ###
 
-    #reward for getting food
-    R1 = [newPosValue]
-    
+    #reward for slow movement and rotation
+    R1 = [1 - (Y[0] / speed)**2, 1 - (Y[1] / speedAngle)**2]
+
+    '''
     #reward for proximity to food
     R2 = [closePrey[1], -closePred[1]]
 
@@ -195,11 +146,57 @@ def process(Y):
 
     #put all rewards together
     R = numpy.concatenate((R1, R2, R3))
+    '''
 
     ### END ###
 
     #return the pair of inputs for next cycle
     return [X, R]
+
+#get cell data
+def getCell(x, y):
+    #use world variable
+    global world
+
+    #check if cell already exists
+    try:
+        return world[(x,y)]
+    except KeyError:
+        #check if anything in cell
+        cellType = numpy.random.random()
+        if cellType > chanceGood + chanceBad:
+            #note that nothing is in the cell
+            world[(x,y)] = None
+            return None
+
+        #check if cell is good or bad
+        cellGood = cellType < chanceGood
+
+        #locate item in cell
+        i = x + grid*numpy.random.random()
+        j = y + grid*numpy.random.random()
+
+        #build cell and return it
+        world[(x,y)] = [(i,j), cellGood]
+        return [(i,j), cellGood]
+
+#activation function
+def nonLinearFunction(x):
+    if x>0:
+        return numpy.log(1+x)
+    else:
+        return -numpy.log(1-x)
+
+#derivative of the activation function
+def nonLinearDerivative(x):
+    if x>0:
+        return 1/(1+x)
+    else:
+        return 1/(1-x)
+
+#make functions work on matrices
+nlf = numpy.vectorize(nonLinearFunction)
+nld = numpy.vectorize(nonLinearDerivative)
 
 #accept commands for stepping speed and exiting
 cmd = ''
@@ -207,10 +204,9 @@ steps = 0
 
 #loop until command Q given
 while True:
-    #draw the world
-    print('OUTPUT: ' + str(lastY))
-    print('REWARD: ' + str(lastR))
+    #draw outputs
     draw()
+    print(world)
 
     #get input for commands
     cmd = input('CMD: ')
@@ -218,11 +214,8 @@ while True:
     #chech command
     if cmd == 'q':
         exit()
-    elif cmd == 'w':
-        print('Wi:')
-        print(Wi)
-        print('Wo:')
-        print(Wo)
+    elif cmd == 's':
+        print('STATE: Network state goes here')
     else:
         #try to adjust step speed
         try:
